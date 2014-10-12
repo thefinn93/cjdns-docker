5648ee3fa3dfb4926838f6380dc2631c089b31b0
Finn <thefinn93@thefinn93.com>
2014-10-12 11:22:59 -0700

    Automatically launch the web UI

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM ubuntu:trusty
 RUN apt-get update
-RUN apt-get install -y nodejs git build-essential python jq
+RUN apt-get install -y nodejs npm git build-essential python jq
 ADD ./installcjdns.sh /tmp/installcjdns.sh
 RUN /tmp/installcjdns.sh
 ADD ./settings /tmp/settings
 ADD ./editconf.py /opt/editconf.py
 ADD ./makeconfig.sh /usr/bin/startcjdns
 
-EXPOSE 11234/udp
 ENTRYPOINT ["/usr/bin/startcjdns"]
--- makeconfig.sh
+++ makeconfig.sh
@@ -9,6 +9,8 @@ CJDROUTECONF="/etc/cjdroute.conf"
 [ -c /dev/net/tun ] || mknod /dev/net/tun c 10 200 ||
     echo "Permission errors making the tun device! Did you run with --privileged?"
 
+IP4=$(ip addr show dev eth0 | grep "inet " | awk '{print $2}' | cut -d'/' -f1)
+
 if ! [ -e $CJDROUTECONF ]; then
     ( # start a subshell to avoid side effects of umask later on
         umask 077 # to create the file with 600 permissions without races
@@ -17,15 +19,22 @@ if ! [ -e $CJDROUTECONF ]; then
     KEY=$(cat $CJDROUTECONF | jq -r .publicKey)
     ADDR=$(cat $CJDROUTECONF | jq -r .ipv6)
     ADMINPW=$(cat /etc/cjdroute.conf | jq .admin.password)
-    IP4=$(ip addr show dev eth0 | grep "inet " | awk '{print $2}' | cut -d'/' -f1)
     echo "NEW CONFIGURATION GENERATED!"
     echo "PUBLIC KEY: $KEY"
     echo "ADDRESS: $ADDR"
     echo "{\"password\": $ADMINPW, \"addr\": \"$IP4\", \"port\": 11234}" | jq .
+    echo "{\"password\": $ADMINPW, \"addr\": \"$IP4\", \"port\": 11234, \"config\": \"/etc/cjdroute.conf\"}" | jq . > /root/.cjdnsadmin
 fi
 
 # This python script preforms a series of operations that to edit the config.
 # All of these are much easier to do in python, hence the language switch
 /opt/editconf.py $CJDROUTECONF
 
-$CJDROUTE --nobg < $CJDROUTECONF
+cd /opt/cjdns/contrib/nodejs/admin
+sed -i "s/'127.0.0.1',//g" /opt/cjdns/contrib/nodejs/admin/admin.js
+npm install
+
+$CJDROUTE < $CJDROUTECONF
+
+echo "Visit http://$IP4:8084/ for HTTP admin interface"
+nodejs /opt/cjdns/contrib/nodejs/admin/admin.js
